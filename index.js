dd484a26e7d4dd4fc46d7cae91c91704e235d63d
Ramana Kumar <ramana@member.fsf.org>
2023-12-05 13:55:31 +0000

    Start filling out page

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 const socket = io()
-const body = document.querySelector('body')
-body.appendChild(document.createElement('p')).innerText = 'hello world'
+const html = document.querySelector('html')
+const body = html.appendChild(document.createElement('body'))
+
+const titleHeading = document.createElement('h1')
+const entryHeading = document.createElement('h2')
+const selectedHeading = document.createElement('h2')
+const perfHeading = document.createElement('h2')
+
+titleHeading.innerText = '🚀 RocketPerv 📉'
+entryHeading.innerText = 'Enter Validators'
+selectedHeading.innerText = 'Selected Validators'
+perfHeading.innerText = 'Performance of Selected Validators'
+
+const entityEntryBox = document.createElement('textarea')
+entityEntryBox.placeholder = 'Node, minipool, or withdrawal addresses/ENS names, and/or validator pubkeys/indices, separated by spaces or commas'
+entityEntryBox.cols = 42
+entityEntryBox.rows = 5
+
+const selectedEntitiesList = document.createElement('ul')
+
+body.append(
+  titleHeading,
+  entryHeading,
+  entityEntryBox,
+  selectedHeading,
+  selectedEntitiesList,
+  perfHeading
+)
--- server.js
+++ server.js
@@ -20,6 +20,21 @@ app.get('/index.js', (req, res) => {
   res.sendFile('index.js', options)
 })
 
+app.get('/index.css', (req, res) => {
+  res.sendFile('index.css', options)
+})
+
+app.get('/icon.png', (req, res) => {
+  res.sendFile('icon.png', options)
+})
+
 const server = http.createServer(app)
 const io = new Server(server)
 server.listen(3000)
+
+io.on('connection', socket => {
+  console.log(`connection: ${socket.id}`)
+  socket.on('disconnect', () => {
+    console.log(`disconnection: ${socket.id}`)
+  })
+})

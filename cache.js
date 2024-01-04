7f7788aadbbe09af7f5d23a85134623e6d52ba24
Ramana Kumar <ramana@member.fsf.org>
2024-01-04 13:36:46 +0000

    Remove worker threads: concurrency within thread instead
    This is better since it's mainly i/o bound

--- cache.js
+++ cache.js
@@ -1,17 +1,44 @@
 import 'dotenv/config'
 import { ethers } from 'ethers'
 import { db, provider, chainId, beaconRpcUrl, log, multicall, secondsPerSlot,
-         timeSlotConvs, slotsPerEpoch, epochOfSlot, minipoolAbi, arrayMin,
+         timeSlotConvs, slotsPerEpoch, epochOfSlot, minipoolAbi, arrayMin, arrayMax,
          minipoolsByPubkeyCount, minipoolsByPubkey, minipoolCount,
          updateMinipoolCount, incrementMinipoolsByPubkeyCount, getIndexFromPubkey,
          getMinipoolByPubkey, getFinalizedSlot, getPubkeyFromIndex,
          rocketMinipoolManager, FAR_FUTURE_EPOCH, epochFromActivationInfo
        } from './lib.js'
-import { Worker } from 'node:worker_threads'
 const {timeToSlot, slotToTime} = timeSlotConvs(chainId)
 
 const MAX_QUERY_RANGE = 1000
 const MAX_BEACON_RANGE = 100
+
+function hexStringToBitvector(s) {
+  const bitlist = []
+  let hexDigits = s.substring(2)
+  if (hexDigits.length % 2 !== 0)
+    hexDigits = `0${hexDigits}`
+  let i
+  while (hexDigits.length) {
+    const byteStr = hexDigits.substring(0, 2)
+    hexDigits = hexDigits.substring(2)
+    const uint8 = parseInt(`0x${byteStr}`)
+    i = 1
+    while (i < 256) {
+      bitlist.push(!!(uint8 & i))
+      i *= 2
+    }
+  }
+  return bitlist
+}
+
+function hexStringToBitlist(s) {
+  const bitlist = hexStringToBitvector(s)
+  let i = bitlist.length
+  while (!bitlist[--i])
+    bitlist.pop()
+  bitlist.pop()
+  return bitlist
+}
 
 let running = true
 
@@ -193,32 +220,259 @@ let processedMinipoolCount = 0
 
 const NUM_WORKERS = parseInt(process.env.NUM_WORKERS) || 2
 
-const workers = Array.from(Array(NUM_WORKERS).keys()).map(
-  i => ({
-    worker: new Worker('./cacheEpochWorker.js'),
-    promise: i,
-    resolveWhenReady: null
-  })
-)
+async function getAttestationDuties(epoch, validatorIds) {
+  log(`Getting attestation duties for ${epoch}`)
 
-workers.forEach((data, i) => {
-  data.worker.on('message', () => {
-    if (typeof data.resolveWhenReady == 'function')
-      data.resolveWhenReady(i)
-  })
-  data.worker.once('error', (e) => {
-    console.error(`Error in worker ${data.worker.threadId}, exiting...`)
-    console.error(e)
-    process.exit(1)
-  })
-})
+  if (!validatorIds.size) {
+    console.warn(`${epoch} has no relevant active validators`)
+    return
+  }
 
-async function getWorker() {
-  const i = await Promise.any(workers.map(({promise}) => promise))
-  const data = workers[i]
-  data.promise = new Promise(resolve => data.resolveWhenReady = resolve)
-  return data.worker
+  const firstSlotInEpoch = epoch * slotsPerEpoch
+
+  const attestationDutiesUrl = new URL(
+    `/eth/v1/beacon/states/${firstSlotInEpoch}/committees?epoch=${epoch}`,
+    beaconRpcUrl
+  )
+
+  if (!running) return
+
+  const logAddedSet = new Set()
+  const logAddedList = []
+  const logAdded = (index, slot) => {
+    logAddedSet.add(index)
+    logAddedList.push(slot)
+  }
+
+  const committees = await fetch(attestationDutiesUrl).then(async res => {
+    if (res.status !== 200)
+      throw new Error(`Got ${res.status} fetching attestation duties for ${epoch}: ${await res.text()}`)
+    const json = await res.json()
+    return json.data
+  })
+  for (const {index, slot, validators} of committees) {
+    if (!running) break
+    for (const [position, selectedIndex] of validators.entries()) {
+      if (!running) break
+      if (validatorIds.has(selectedIndex)) {
+        const attestationKey = `${chainId}/validator/${selectedIndex}/attestation/${epoch}`
+        const attestation = db.get(attestationKey) || {}
+        if (!('position' in attestation)) {
+          // log(`Adding attestation duty @ ${slot} for validator ${selectedIndex}`)
+          logAdded(selectedIndex, slot)
+          attestation.slot = parseInt(slot)
+          attestation.index = parseInt(index)
+          attestation.position = position
+          await db.put(attestationKey, attestation)
+        }
+      }
+    }
+  }
+
+  if (logAddedList.length != logAddedSet.size)
+    throw new Error(`Unexpected difference in logAdded: ${logAddedList.length} vs ${logAddedSet.size}`)
+  log(`Added ${logAddedSet.size} attestation duties in epoch ${epoch} between ${arrayMin(logAddedList.slice())} and ${arrayMax(logAddedList)}`)
 }
+
+async function processEpoch(epoch, validatorIds) {
+  log(`Processing epoch ${epoch}`)
+
+  if (!validatorIds.size) {
+    console.warn(`${epoch} has no relevant active validators`)
+    return
+  }
+
+  const rewardsOptions = rewardsOptionsForEpoch(validatorIds)
+
+  const firstSlotInEpoch = epoch * slotsPerEpoch
+
+  if (!running) return
+  log(`Getting sync duties for ${epoch}`)
+
+  const syncDutiesUrl = new URL(
+    `/eth/v1/beacon/states/${firstSlotInEpoch}/sync_committees?epoch=${epoch}`,
+    beaconRpcUrl
+  )
+  const syncValidators = await fetch(syncDutiesUrl).then(async res => {
+    if (res.status === 400 && await res.json().then(j => j.message.endsWith("not activated for Altair")))
+      return []
+    if (res.status !== 200)
+      throw new Error(`Got ${res.status} fetching sync duties for ${epoch}: ${await res.text()}`)
+    const json = await res.json()
+    return json.data.validators
+  })
+  for (const [position, validatorIndex] of syncValidators.entries()) {
+    if (validatorIds.has(validatorIndex)) {
+      const syncKey = `${chainId}/validator/${validatorIndex}/sync/${epoch}`
+      const sync = db.get(syncKey) || {}
+      if (!('position' in sync)) {
+        log(`Adding sync duty for epoch ${epoch} for validator ${validatorIndex}`)
+        sync.position = position
+        sync.missed = []
+        sync.rewards = []
+        await db.put(syncKey, sync)
+      }
+    }
+  }
+
+  if (!running) return
+  log(`Getting attestations and syncs for ${epoch}`)
+
+  validatorIds.forEach(validatorIndex => {
+    if (!(epoch <= db.get(`${chainId}/validator/${validatorIndex}/dutiesEpoch`)))
+      throw new Error(`dutiesEpoch for ${validatorIndex} too low for ${epoch}`)
+  })
+
+  let searchSlot = firstSlotInEpoch
+  while (running && searchSlot < firstSlotInEpoch + slotsPerEpoch) {
+    const blockUrl = new URL(
+      `/eth/v1/beacon/blinded_blocks/${searchSlot}`,
+      beaconRpcUrl
+    )
+    const blockData = await fetch(blockUrl).then(async res => {
+      if (res.status === 404) {
+        log(`Block for slot ${searchSlot} missing`)
+        return { attestations: [] }
+      }
+      if (res.status !== 200)
+        throw new Error(`Got ${res.status} fetching blinded block @ ${searchSlot}: ${await res.text()}`)
+      const json = await res.json()
+      return json.data.message.body
+    })
+    const attestations = blockData.attestations
+    for (const {aggregation_bits, data: {slot, index, beacon_block_root, source, target}} of attestations) {
+      const attestedBits = hexStringToBitlist(aggregation_bits)
+      const attestationEpoch = epochOfSlot(parseInt(slot))
+      for (const validatorIndex of validatorIds.keys()) {
+        const attestationKey = `${chainId}/validator/${validatorIndex}/attestation/${attestationEpoch}`
+        const attestation = db.get(attestationKey)
+        if (attestation?.slot == slot && attestation.index == index && !(attestation.attested?.slot <= searchSlot)) {
+          if (attestedBits[attestation.position]) {
+            attestation.attested = { slot: searchSlot, head: beacon_block_root, source, target }
+            log(`Adding attestation for ${slot} (${attestationEpoch}) for validator ${validatorIndex} @ ${searchSlot}`)
+            await db.put(attestationKey, attestation)
+          }
+        }
+      }
+    }
+    if (blockData.sync_aggregate) {
+      const syncBits = hexStringToBitvector(blockData.sync_aggregate.sync_committee_bits)
+      for (const validatorIndex of validatorIds.keys()) {
+        const syncKey = `${chainId}/validator/${validatorIndex}/sync/${epoch}`
+        const sync = db.get(syncKey)
+        if (sync && !syncBits[sync.position] && !sync.missed.includes(searchSlot)) {
+          sync.missed.push(searchSlot)
+          await db.put(syncKey, sync)
+        }
+      }
+
+      const syncRewardsUrl = new URL(
+        `/eth/v1/beacon/rewards/sync_committee/${searchSlot}`,
+        beaconRpcUrl
+      )
+      const syncRewards = await fetch(syncRewardsUrl, rewardsOptions).then(async res => {
+        if (res.status !== 200)
+          throw new Error(`Got ${res.status} fetching sync rewards @ ${searchSlot}: ${await res.text()}`)
+        const json = await res.json()
+        return json.data
+      })
+      for (const {validator_index, reward} of syncRewards) {
+        const syncKey = `${chainId}/validator/${validator_index}/sync/${epoch}`
+        const sync = db.get(syncKey)
+        if (!sync) {
+          if (reward !== '0')
+            throw new Error(`Non-zero reward ${reward} but no sync object at ${syncKey}`)
+          continue
+        }
+        if (sync.rewards.every(({slot}) => slot != searchSlot)) {
+          log(`Adding sync reward for ${searchSlot} for validator ${validator_index}: ${reward}`)
+          sync.rewards.push({slot: searchSlot, reward})
+          await db.put(syncKey, sync)
+        }
+      }
+    }
+
+    searchSlot++
+  }
+
+  if (!running) return
+  log(`Getting attestation rewards for ${epoch}`)
+
+  const attestationRewardsUrl = new URL(
+    `/eth/v1/beacon/rewards/attestations/${epoch}`,
+    beaconRpcUrl
+  )
+  const attestationRewards = await fetch(attestationRewardsUrl, rewardsOptions).then(async res => {
+    if (res.status !== 200)
+      throw new Error(`Got ${res.status} fetching attestation rewards in epoch ${epoch}: ${await res.text()}`)
+    const json = await res.json()
+    return json.data
+  })
+  for (const {validator_index, head, target, source, inactivity} of attestationRewards.total_rewards) {
+    if (!running) break
+    const attestationKey = `${chainId}/validator/${validator_index}/attestation/${epoch}`
+    const attestation = db.get(attestationKey)
+    if (attestation && !('reward' in attestation && 'ideal' in attestation)) {
+      const effectiveBalanceUrl = new URL(
+        `/eth/v1/beacon/states/${firstSlotInEpoch}/validators/${validator_index}`,
+        beaconRpcUrl
+      )
+      const effectiveBalance = await fetch(effectiveBalanceUrl).then(async res => {
+        if (res.status !== 200)
+          throw new Error(`Got ${res.status} fetching effective balance in ${firstSlotInEpoch} for ${validator_index}: ${await res.text()}`)
+        const json = await res.json()
+        return json.data.validator.effective_balance
+      })
+      attestation.reward = {head, target, source, inactivity}
+      const ideal = attestationRewards.ideal_rewards.find(x => x.effective_balance === effectiveBalance)
+      if (!ideal) throw new Error(`Could not get ideal rewards for ${firstSlotInEpoch} ${validator_index} with ${effectiveBalance}`)
+      attestation.ideal = {}
+      for (const key of Object.keys(attestation.reward))
+        attestation.ideal[key] = ideal[key]
+      log(`Adding attestation reward for epoch ${epoch} for validator ${validator_index}: ${Object.entries(attestation.reward)} / ${Object.entries(attestation.ideal)}`)
+      await db.put(attestationKey, attestation)
+    }
+  }
+
+  if (!running) return
+  log(`Getting proposals for ${epoch}`)
+
+  const proposalUrl = new URL(
+    `/eth/v1/validator/duties/proposer/${epoch}`,
+    beaconRpcUrl
+  )
+  const proposals = await fetch(proposalUrl).then(async res => {
+    if (res.status !== 200)
+      throw new Error(`Got ${res.status} fetching proposal duties for epoch ${epoch}: ${await res.text()}`)
+    const json = await res.json()
+    return json.data
+  })
+  for (const {validator_index, slot} of proposals) {
+    if (!running) break
+    if (validatorIds.has(validator_index)) {
+      const proposalKey = `${chainId}/validator/${validator_index}/proposal/${slot}`
+      const proposal = db.get(proposalKey) || {}
+      if (!('reward' in proposal)) {
+        const proposalRewardUrl = new URL(`/eth/v1/beacon/rewards/blocks/${slot}`, beaconRpcUrl)
+        const response = await fetch(proposalRewardUrl)
+        if (response.status === 404) {
+          log(`Adding missed proposal for ${validator_index} @ ${slot}`)
+          proposal.missed = true
+          proposal.reward = '0'
+        }
+        else if (response.status === 200) {
+          log(`Adding proposal reward for ${validator_index} @ ${slot}`)
+          const reward = await response.json().then(j => j.data.total)
+          proposal.reward = reward
+        }
+        else throw new Error(`Got ${response.status} fetching block rewards @ ${slot}`)
+        await db.put(proposalKey, proposal)
+      }
+    }
+  }
+}
+
+const tasks = []
 
 async function processEpochsLoop(finalizedSlot, dutiesOnly) {
   const uptoKey = dutiesOnly ? `dutiesEpoch` : `nextEpoch`
@@ -245,8 +499,8 @@ async function processEpochsLoop(finalizedSlot, dutiesOnly) {
     log(`${epochsToProcess.length} epochs left to process ${processMsg}`)
     const epoch = epochsToProcess.shift()
     const validatorIds = getValidatorsIdsForEpoch(epoch)
-    const worker = await getWorker()
-    worker.prependOnceListener('message', async () => {
+    const state = {}
+    const onCompletion = async () => {
       const epochIndex = pendingEpochs.indexOf(epoch)
       pendingEpochs.splice(epochIndex, 1)
       if (epochIndex == 0) {
@@ -262,8 +516,22 @@ async function processEpochsLoop(finalizedSlot, dutiesOnly) {
         if (updated.length)
           log(`Updated ${uptoKey} to ${epoch} for ${updated.length} validators from ${updated.at(0)} to ${updated.at(-1)}`)
       }
+      state.resolved = true
+      log(`Task for ${epoch} completed`)
+    }
+    const fn = dutiesOnly ? getAttestationDuties : processEpoch
+    tasks.push({
+      state, task: fn(epoch, validatorIds).then(onCompletion)
     })
-    worker.postMessage({epoch, validatorIds, dutiesOnly})
+    while (tasks.length >= NUM_WORKERS) {
+      await Promise.race(tasks.map(({task}) => task))
+      let i = 0
+      for (const {state, task} of tasks) {
+        if (!state.resolved)
+          tasks[i++] = {state, task}
+      }
+      tasks.length = i
+    }
   }
 }
 
@@ -314,16 +582,8 @@ process.on('SIGINT', async () => {
   running = false
   log(`Removing listeners...`)
   await provider.removeAllListeners('block')
-  log(`Terminating workers...`)
-  workers.forEach(({worker}) => worker.postMessage({interrupt: true}))
-  await Promise.all(
-    workers.map(({worker, promise}, i) =>
-      promise.then(() => {
-        log(`Worker ${i} finished`)
-        return worker.terminate()
-      })
-    )
-  )
+  log(`Awaiting tasks...`)
+  await Promise.allSettled(tasks)
   log(`Closing db...`)
   await db.close()
   process.exit()

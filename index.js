2c74146c88c7d14f12ab9fa8623ea22925b566af
Ramana Kumar <ramana@member.fsf.org>
2024-01-17 22:42:24 +0000

    Add simple polyfill for toSpliced

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 // @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
+
+const hasToSpliced = Array.prototype.hasOwnProperty('toSpliced')
+console.log(`toSpliced: ${hasToSpliced}`)
+if (!hasToSpliced) {
+  Array.prototype.toSpliced = function (...args) {
+    const copy = this.slice()
+    copy.splice(...args)
+    return copy
+  }
+}
+
 const socket = io()
 const body = document.querySelector('body')
 

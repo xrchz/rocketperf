1519e257036a9a6886ad4ac7c0ac4630390d19f3
Ramana Kumar <ramana@member.fsf.org>
2024-02-01 07:17:23 +0000

    Apply same fix to moveToFreshDb, and incorporate migration too

--- moveToFreshDb.js
+++ moveToFreshDb.js
@@ -5,32 +5,70 @@ const newDbPath = '/home/ramana/lh/db'
 const oldDb = open({path: oldDbPath})
 const newDb = open({path: newDbPath})
 const chainId = 1
+/*
 const startValidator = parseInt(process.env.START)
 const endValidator = parseInt(process.env.END)
 if (!(0 <= startValidator && startValidator < endValidator))
   throw new Error(`Bad START and END`)
 const start = [chainId, 'validator', startValidator]
 const end = [chainId, 'validator', endValidator]
+*/
 /*
 const start = [chainId, 'w']
 // const end = [chainId, 'validator', '']
-const end = undefined
 */
+// Up to 1,validator,271871,attestation,219546, skipped 300000000 already existing...
+// Up to 1,validator,275213,attestation,117673, skipped 43600000 already existing...
+const start = [1,'validator',275213]
+const end = undefined
+let existCount = 0n
 const batchSize = parseInt(process.env.BS) || 8192
 const batch = []
+async function clearBatch() {
+  const {key: lastKey} = batch.at(-1)
+  await newDb.transaction(() => {
+    for (const {newKey, newValue} of batch.splice(0, Infinity)) {
+      newDb.put(newKey, newValue)
+    }
+  })
+  console.log(`Replaced up to ${lastKey}`)
+}
 for (const {key, value} of oldDb.getRange({start, end})) {
-  if (newDb.doesExist(key)) {
-    console.warn(`${key} already exists in ${newDbPath}: skipping`)
+  let newKey = key
+  if (newDb.doesExist(newKey)) {
+    if (++existCount % 100000n == 0n)
+      console.log(`Up to ${key}, skipped ${existCount} already existing...`)
     continue
   }
-  batch.push({key, value})
-  if (batch.length >= batchSize) {
-    const {key: lastKey} = batch.at(-1)
-    await newDb.transaction(() => {
-      for (const {key, value} of batch.splice(0, Infinity)) {
-        newDb.put(key, value)
-      }
-    })
-    console.log(`Replaced up to ${lastKey}`)
+  let newValue = value
+  if (typeof key == 'string') {
+    newKey = key.split('/')
+    if (newKey[0] != chainId) throw new Error(`Unexpected chainId ${newKey[0]}`)
+    newKey[0] = parseInt(chainId)
+    for (let i = 1; i < newKey.length; i++) {
+      if (newKey[i] === 'validator' || newKey[i] === 'attestation' || newKey[i] === 'sync' || newKey[i] === 'proposal')
+        newKey[i+1] = parseInt(newKey[++i])
+      else if ((newKey[i] === 'nextEpoch' || newKey[i] === 'dutiesEpoch') && i+1 < newKey.length)
+        newKey[i+1] = parseInt(newKey[++i])
+    }
   }
+  if (newDb.doesExist(newKey)) {
+    if (++existCount % 100000n == 0n)
+      console.log(`Up to ${key}, skipped ${existCount} already existing...`)
+    continue
+  }
+  if (value instanceof Set) {
+    console.log(`Replacing Set for ${key}`)
+    newValue = Array.from(value.keys())
+  }
+  if (value instanceof Map) {
+    console.log(`Replacing Map for ${key}`)
+    newValue = {}
+    for (const [k, v] of value.entries()) {
+      newValue[k] = v
+    }
+  }
+  batch.push({key, newKey, newValue, value})
+  if (batch.length >= batchSize) await clearBatch()
 }
+if (batch.length) await clearBatch()
